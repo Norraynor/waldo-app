1de68c20ef9daea0b3c348709463a801bbd221eb
norraynor <alechanderr@gmail.com>
2022-02-21 18:35:35 +0100

    initial setup firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC1TvsWFWP8R4zU5aT8KYG0gJxXlD5H6Ow",
+  authDomain: "waldo-app-20884.firebaseapp.com",
+  projectId: "waldo-app-20884",
+  storageBucket: "waldo-app-20884.appspot.com",
+  messagingSenderId: "773298284070",
+  appId: "1:773298284070:web:25eb23b3c973e46f81e8e7"
+})
+
+const firestore = firebase.firestore();
 
 function App() {
   return (
