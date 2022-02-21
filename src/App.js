3e9a1c4e93d13ad08be625c9233c94d09275dc1a
norraynor <alechanderr@gmail.com>
2022-02-21 18:30:15 +0100

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        LMAO
+        <div className='img-container'>image goes here - with logic and stuff</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
