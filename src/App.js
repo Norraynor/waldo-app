f1eb204859fe053168816edf558b95c59f9203d5
norraynor <alechanderr@gmail.com>
2022-02-27 13:02:47 +0100

    added wally object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Photo from "./components/Photo.js";
 
@@ -19,13 +19,46 @@ firebase.initializeApp({
 const firestore = firebase.firestore();
 
 function App() {
+  const [mousePos,setMousePos]= useState([]);
+
   return (
     <div className="App">
+      <div id="mouse-circle"></div>
+
+      {
+        //for testing mouse radius
+        document.addEventListener('DOMContentLoaded', () => {
+          let mousePosX = 0, mousePosY = 0,
+          mouseCircle = document.getElementById('mouse-circle');
+
+        document.onmousemove = (e) => {
+          mousePosX = e.pageX;
+          mousePosY = e.pageY;
+        }
+
+        let delay = 6,
+        revisedMousePosX = 0,
+        revisedMousePosY = 0;
+        function delayMouseFollow() {
+          requestAnimationFrame(delayMouseFollow);
+
+          revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
+          revisedMousePosY += (mousePosY - revisedMousePosY) / delay; 
+
+          mouseCircle.style.top = revisedMousePosY + 'px';
+          mouseCircle.style.left = revisedMousePosX + 'px';
+
+          setMousePos([revisedMousePosX,revisedMousePosY]);
+        }
+        delayMouseFollow();
+        })
+      }
+
       <header className="App-header">
         LMAO
         <div className='img-container'>
           image goes here - with logic and stuff
-          <Photo />
+          <Photo mousePosX={mousePos[0]} mousePosY={mousePos[1]}/>
         </div>
       </header>
     </div>
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,16 +1,36 @@
 import './Photo.css';
 
+const wally = {
+  x:820,
+  y:290,
+  width:20,
+  height:40
+}
 function Photo(props) { 
     function handleMouseClick(event){
-        let rect = event.target.getBoundingClientRect();
-        //console.log(event.clientX,event.clientY,event.pageX,event.pageY,event.screenX,event.screenY,rect)
-        let x = event.clientX - rect.left;
-        let y = event.clientY - rect.top;
-        let currentPosition = [x,y];
-        console.log(currentPosition);
+      let rect = event.target.getBoundingClientRect();
+      //console.log(event.clientX,event.clientY,event.pageX,event.pageY,event.screenX,event.screenY,rect)
+      let x = event.clientX - rect.left;
+      let y = event.clientY - rect.top;
+      let currentPosition = [x,y];
+      console.log(currentPosition);
+
+
+      //collision detection could be viable here https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Collision_detection
+      collisionDetection();
+    }
+    function collisionDetection(){
+      console.log(Math.round(props.mousePosX),Math.round(props.mousePosY));
+      if(props.mousePosX>wally.x && props.mousePosX<wally.x+wally.width && props.mousePosY>wally.y && props.mousePosY<wally.y+wally.height){
+        //hit wally
+        console.log("wally hit");
+      }else{
+        console.log("missed");
+      }
     }
   return (
     <div className="Photo" onClick={handleMouseClick} >
+      <div className='wally'></div>
         Photo goes here!
     </div>
   );
